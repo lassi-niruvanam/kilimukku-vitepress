6bac0fbad8ea26b97a9f40508677612fbc3f3157
julienmalard <julien.malard@mail.mcgill.ca>
2023-11-08 17:10:09 +0100

    Exporter extentions

---
+++ src/extentions/index.ts
@@ -0,0 +1,5 @@
+export { ExtentionImages } from "./images.js";
+export { Extention, ExtentionDéfaut } from "./extention.js";
+export { ExtentionMd } from "./md.js";
+export { ExtentionSvg } from "./svg.js";
+export { ExtentionYaml } from "./yaml.js";
