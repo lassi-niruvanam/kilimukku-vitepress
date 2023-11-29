5232109b3f0844af59cfab04a0bd992545e32d94
julienmalard <julien.malard@mail.mcgill.ca>
2023-11-29 14:24:38 +0100

    Tests

--- test/extentions/md.spec.ts
+++ test/extentions/md.spec.ts
@@ -1,9 +1,31 @@
-import {expect} from "aegir/chai"
+import { ExtentionMd } from "@/extentions/md.js"
+import { empreinte } from "@/utils.js"
 
-describe(
-    "Extention md", function () {
-        it("", async () => {
-            
+import {expect} from "aegir/chai"
+import { marked } from "marked"
+
+describe("Extention md", function () {
+    describe("Analyser composantes", function () {
+        const extention = new ExtentionMd()
+        it("Entête", async () => {
+            const composante = marked.lexer("# Un titre\n")[0]
+            const rés = extention.analyserComposante({composante})
+            expect(rés).to.deep.equal([{clef: '', valeur: 'Un titre'}])
         })
-    }
-)
+    })
+
+    describe("Reconstruire composantes", function () {
+        const extention = new ExtentionMd()
+        it("Entête", async () => {
+            const fichier = "fichier test"
+            const composante = marked.lexer("# Un titre\n")[0]
+            const rés = extention.reconstruireComposante({
+                composante,
+                traducs: {[fichier + "." + empreinte("Un titre")]: "Un título"},
+                langue: "es",
+                fichier
+            })
+            expect(rés).to.equal("# Un título\n")
+        })
+    })
+})
